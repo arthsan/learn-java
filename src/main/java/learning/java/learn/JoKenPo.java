2832e6f30706a01a8eb4535035bdf91c02cf60cd
Arthur Santoianni Barazzone Durant <arthur.durant@latoex.corp>
2019-08-16 10:40:47 -0300

    jokenpo

---
+++ src/main/java/learning/java/learn/JoKenPo.java
@@ -0,0 +1,42 @@
+package learning.java.learn;
+
+import java.util.Random;
+
+public class JoKenPo {
+
+    public static void main(String[] args) {
+
+        // rock = 0
+        // scissor = 1
+        // paper = 2
+
+        Random dice = new Random();
+        int p1 = dice.nextInt(3);
+        int p2 = dice.nextInt(3);
+
+        if (p1 == 0) {
+            System.out.println("Player 1 = " + "Rock");
+        } else if (p1 == 1) {
+            System.out.println("Player 1 = " + "Scissor");
+        } else {
+            System.out.println("Player 1 = " + "Paper");
+        }
+
+        if (p2 == 0) {
+            System.out.println("Player 2 = " + "Rock");
+        } else if (p2 == 1) {
+            System.out.println("Player 2 = " + "Scissor");
+        } else {
+            System.out.println("Player 2 = " + "Paper");
+        }
+
+        if (p1 == p2) {
+            System.out.println("Draw");
+        } else if ((p1 == 0 && p2 == 1) || (p1 == 1 && p2 == 2) || (p1 == 2 && p2 == 0)) {
+            System.out.println("Player 1 won!");
+        } else {
+            System.out.println("Player 2 won!");
+        }
+    }
+
+}
