8ac452bce750b03a004366eb0ab83b13fb837eba
Arthur Santoianni Barazzone Durant <arthur.durant@latoex.corp>
2019-08-16 12:19:06 -0300

    raffleapp

--- src/main/java/learning/java/learn/RaffleApp.java
+++ src/main/java/learning/java/learn/RaffleApp.java
@@ -1,4 +1,16 @@
 package learning.java.learn;
 
+import java.util.Random;
+
 public class RaffleApp {
+
+    public static void main(String[] args) {
+
+        Random dice = new Random();
+        String people[] = {"Arthur" , "Daniela", "Raphael", "Filipe", "Rodrigo", "Karen", "Keila"};
+        int raffle = dice.nextInt(people.length);
+        String winner = people[raffle];
+
+        System.out.println("Winner is: " + winner);
+    }
 }
